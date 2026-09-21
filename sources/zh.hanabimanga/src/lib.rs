4c98bfe1b1c7a8bba97ba34bfc8aaecd64b1f2d5
DeAthBo <584556872@qq.com>
2026-09-21 14:34:43 +0800

    fix(zh.hanabimanga): propagate login request error

--- sources/zh.hanabimanga/src/lib.rs
+++ sources/zh.hanabimanga/src/lib.rs
@@ -215,7 +215,7 @@ impl HanabiManga {
 			gotrue_meta_security: MetaSecurity {},
 		};
 		let json = serde_json::to_vec(&body).ok()?;
-		let result: LoginResult = post_json("/auth/v1/token?grant_type=password", &json)
+		let result: LoginResult = post_json("/auth/v1/token?grant_type=password", &json)?
 			.json_owned()
 			.ok()?;
 
