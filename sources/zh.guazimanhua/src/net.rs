cbf492e4ac874b48f226558a07f0e4cd419e56dd
DeAthBo <584556872@qq.com>
2026-09-21 11:22:56 +0800

    fix(zh.guazimanhua): wrap request in Ok

--- sources/zh.guazimanhua/src/net.rs
+++ sources/zh.guazimanhua/src/net.rs
@@ -22,8 +22,8 @@ pub enum Url<'a> {
 
 impl Url<'_> {
 	pub fn request(&self) -> Result<Request> {
-		Request::get(self.to_string())?
-			.header("User-Agent", USER_AGENT)
+		Ok(Request::get(self.to_string())?
+			.header("User-Agent", USER_AGENT))
 	}
 }
 
